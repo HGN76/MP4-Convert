fe6ba0f6866ba2c73a3cb33bc5af4db8b8a5259f
HGN76 <HGN76@users.noreply.github.com>
2016-06-30 11:45:39 +0200

    Update mp4-convert.sh

--- mp4-convert.sh
+++ mp4-convert.sh
@@ -5,10 +5,22 @@
 # Description: Convert Full HD MP4's to a specific resolution [in this case 1080x608] using HandBrakeCL
 # This script depends on the following command line tool: HandBrakeCLI    http://handbrake.fr/
 
-SRC=/home/user/Videos/Input/
-DEST=/home/user/Videos/Output
+echo "*********************************************************"
+echo "*                                                       *"
+echo "*                  HandbrakeCLI Script                  *"
+echo "*                                                       *"
+echo "* Full-HD [1920x1080] to Terminal resolution [1080x608] *"
+echo "*                                                       *"
+echo "*********************************************************"
+
+SRC="/home/user/Videos/Input/"
+DEST="/home/user/Videos/Output"
 DEST_EXT=mp4
-HANDBRAKE_CLI=/usr/bin/HandBrakeCLI
+DATE=$(date +"%Y-%m-%d:%H:%M:%S")
+
+# HandBrakeCLI Guide: https://trac.handbrake.fr/wiki/CLIGuide
+HANDBRAKE_CLI="/usr/bin/HandBrakeCLI"
+HANDBRAKE_SETTINGS="--optimize --encoder x264 --x264-preset veryslow --x264-tune film --x264-profile baseline --encoder-level 3.1 --quality 18 --cfr --audio none --width 1080 --height 608 --crop 0:0:0:0 --maxHeight 608 --maxWidth 1080 --loose-anamorphic --modulus 2"
 
 for line in $(find $SRC -name "*.$DEST_EXT")
 do
@@ -20,9 +32,10 @@ do
 	# echo $FOLDER
 	mkdir $DEST/$FOLDER/
 
-	# HandBrakeCLI Guide: https://trac.handbrake.fr/wiki/CLIGuide
-	$HANDBRAKE_CLI --input $line --output $DEST/$FOLDER/$filename.$DEST_EXT --optimize --encoder x264 --x264-preset veryslow --x264-tune film --x264-profile baseline --encoder-level 3.1 --quality 18 --cfr --audio none --width 1080 --height 608 --crop 0:0:0:0 --maxHeight 608 --maxWidth 1080 --loose-anamorphic --modulus 2
+	$HANDBRAKE_CLI --input $line --output $DEST/$FOLDER/$filename.$DEST_EXT $HANDBRAKE_SETTINGS 2> "$DATE-mp4-convert.log"
 
 	# remove Input File
-	rm $line
+	#rm $line
+
+	echo "Job finished. Excellent ;-)"
 done
